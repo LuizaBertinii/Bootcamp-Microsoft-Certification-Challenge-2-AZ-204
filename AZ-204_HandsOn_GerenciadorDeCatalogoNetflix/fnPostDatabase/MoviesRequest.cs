55b681f7f1e2ca12f383958c2f90ab41f3fae183
Maria Luiza Bertini <maria.bertini@animaeducacao.com.br>
2025-02-02 18:04:08 -0300

    fnPostDatabase e fnPostDataStorage

---
+++ AZ-204_HandsOn_GerenciadorDeCatalogoNetflix/fnPostDatabase/MoviesRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace fnPostDatabase
+{
+    internal class MovieRequest
+    {
+        public string Id { get { return Guid.NewGuid().ToString(); } }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Video { get; set; }
+        public string Thumb { get; set; }
+    }
+}
